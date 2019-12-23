1453610d1410921b25573e6138b1823aba42584c
Ntn172 <nitindhiman1712@gmail.com>
2019-12-23 23:19:15 +0530

    first page

--- app.js
+++ app.js
@@ -2,21 +2,13 @@ var puppeteer = require("puppeteer");
 var cherrio = require("cheerio");
 
 const sample = {
+    price: 1000,
     guests: 1,
     bedrooms: 1,
     beds: 1,
     baths: 1,
-    price: 1550,
 };
 
-
-// https://www.airbnb.co.in/s/india/homes?refinement_paths%5B%5D=%2Fhomes&screen_size=large&search_type=search_query
-
-// https://www.airbnb.co.in/s/india/homes?refinement_paths%5B%5D=%2Fhomes&current_tab_id=home_tab&selected_tab_id=home_tab&screen_size=large&search_type=pagination&place_id=ChIJkbeSa_BfYzARphNChaFPjNc&s_tag=CSmPdSok&hide_dates_and_guests_filters=false&last_search_session_id=acd5389c-f895-4728-bdd5-b0c48d6bf782
-
-// https://www.airbnb.co.in/s/india/homes?refinement_paths%5B%5D=%2Fhomes&current_tab_id=home_tab&selected_tab_id=home_tab&screen_size=large&search_type=pagination&place_id=ChIJkbeSa_BfYzARphNChaFPjNc&s_tag=CSmPdSok&hide_dates_and_guests_filters=false&{`section_offset=4&items_offset=18`}&last_search_session_id=2c80cd67-94f6-422b-b758-090d25113219
-
-// https://www.airbnb.co.in/s/india/homes?refinement_paths%5B%5D=%2Fhomes&screen_size=large&search_type=pagination&place_id=ChIJkbeSa_BfYzARphNChaFPjNc&s_tag=CSmPdSok&{`section_offset=4&items_offset=18`}&last_search_session_id=c9247eb5-aa14-4742-8eb8-100ee030f2e9
 
 const uri = "https://www.airbnb.co.in/s/india/homes?refinement_paths%5B%5D=%2Fhomes&current_tab_id=home_tab&selected_tab_id=home_tab&screen_size=large&search_type=pagination&place_id=ChIJkbeSa_BfYzARphNChaFPjNc&s_tag=CSmPdSok&hide_dates_and_guests_filters=false";
 
@@ -25,7 +17,7 @@ let browser;
 async function scrape(url){
     try{
         const page = await browser.newPage();
-        await page.goto(url);
+        await page.goto(url, { waitUntil: "networkidle2" });
         const html = await page.evaluate(() => document.body.innerHTML);
         const $ = await cherrio.load(html);
         const home = $('[itemprop="itemListElement"] > div > div._ylefn59 > a ').map((i, element) => {
@@ -39,10 +31,30 @@ async function scrape(url){
 
 async function scrapeDesc(url, page){
     try{
-        await page.goto(url);
+        await page.goto(url, { waitUntil: "networkidle2" });
+        const html = await page.evaluate(() => document.body.innerHTML);
+        const $ = await cherrio.load(html);
+        const price = $("#room > div > div > div > div > div > div > div > div > div > div > div > div > div > div > div > div > div > div > div > span > span").text();
+        const roomText = $("#room").text();
+        const guestsAllowed = returnMatch(roomText, /\d+ guest/);
+        const bedrooms = returnMatch(roomText, /\d+ bedroom/);
+        const baths = returnMatch(roomText, /\d+ (shared )?bath/);
+        const beds = returnMatch(roomText, /\d+ bed/);
+        return {url, price, guestsAllowed, bedrooms, baths, beds};
     } catch(err){
         console.error(err);
     }
+}
+
+function returnMatch(roomText, regex){
+    const regexMatch = roomText.match(regex);
+    let results = "N/A";
+    if(regexMatch.length != null){
+        results = regexMatch[0];
+    } else {
+        throw 'No regext match';
+    }
+    return results;
 }
 
 async function main(){
@@ -50,9 +62,9 @@ async function main(){
     const descPage = await browser.newPage();
     const home = await scrape(uri);
     for(var i =0; i<home.length;i++){
-        await scrapeDesc(home[i], descPage);
+        const result = await scrapeDesc(home[i], descPage);
+        console.log(result);
     }
-    console.log(home);
 }
 
 main();
